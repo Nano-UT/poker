8a2a41dc810ee757e5c22683ce40b8703b4fb2d8
なの <hironaga1031@yahoo.co.jp>
2017-09-03 05:38:32 +0900

    some small points fixed, not previous problems

--- player.py
+++ player.py
@@ -1,6 +1,15 @@
 from hand import num, suit, card, deck, hand, pocketboard
 
-class player(HandBoard):
+class player(pocketboard):
     def __init__(self,pocket,board):
         pobo = pocket + board
-        self = pobo
+        self._player = pobo
+
+    def __repr__(self):
+        return "{}".format(self._player)
+
+class players(player,n):
+    def __init__(self):
+        self._fald = []
+        for i in range(n):
+            self._fald.append(False)
